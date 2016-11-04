36c58b9a83e80a42ba0018b3526488616b1b57a4
Ronnie Chong <ronnie@otakuness.com>
2016-11-04 23:32:12 +1100

    Unused components

--- src/lib/localtunnel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import config from '../config';
-import LocalTunnel from 'localtunnel';
-
-let localtunnel = LocalTunnel(config.localtunnel.port, {subdomain: config.localtunnel.subdomain},function(err, tunnel) {
-    console.log('Local Tunnel is running ', localtunnel.url, ' using port ', config.localtunnel.port);
-});
-
-export default localtunnel;
